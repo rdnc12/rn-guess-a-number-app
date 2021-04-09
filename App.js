4cdddbf857f76246b2df6aa00811a936f104a785
Rdnc12 <rdnc12@gmail.com>
2021-04-09 18:46:11 +0200

    GameScreen component

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import Header from './components/Header';
-import StartGameScreen from './components/screens/StartGameScreen'
-
+import Header from "./components/Header";
+import StartGameScreen from "./components/screens/StartGameScreen";
+import GameScreen from "./components/screens/GameScreen";
 
 export default function App() {
+
+
   return (
-    <View style={styles.screen} >
-      <Header title="Guess a number"/>
-      <StartGameScreen/>
+    <View style={styles.screen}>
+      <Header title="Guess a number" />
+      <StartGameScreen />
+      <GameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen:{
-    flex:1
-  }
+  screen: {
+    flex: 1,
+  },
 });
