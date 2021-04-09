53c08d127f62f6aafba7c09ca17afd67a2e1117f
Rdnc12 <rdnc12@gmail.com>
2021-04-09 15:32:42 +0200

    showing alert

--- components/screens/StartGameScreen.js
+++ components/screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
 
 import Card from "../Card";
@@ -28,7 +29,12 @@ const StartGameScreen = ({}) => {
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
-    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
       return;
     }
     setConfirmed(true);
