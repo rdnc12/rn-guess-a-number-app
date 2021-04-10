7dbac1f079a610ff3c587f8cc29d0e51f55cdaf2
Rdnc12 <rdnc12@gmail.com>
2021-04-10 12:41:59 +0200

    showing hint and validation

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, Button, StyleSheet, Alert } from "react-native";
 
 import NumberContainer from "../NumberContainer";
 import Card from "../Card";
@@ -21,13 +21,42 @@ const GameScreen = ({ userChoice }) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, userChoice)
   );
+
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userChoice) ||
+      (direction === "greater" && currentGuess > userChoice)
+    ) {
+      Alert.alert("Don/'t lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      currentHigh.current = currentGuess; // useRef doesn't re-render, if changes but useState re-renders
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber); // now component will be re-rendered and it will output the next.
+  };
   return (
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="Lower" onPress={() => {}} />
-        <Button title="Greater" onPress={() => {}} />
+        <Button title="Lower" onPress={nextGuessHandler.bind(this, "lower")} />
+        <Button
+          title="Greater"
+          onPress={nextGuessHandler.bind(this, "greater")}
+        />
       </Card>
     </View>
   );
@@ -49,3 +78,7 @@ const styles = StyleSheet.create({
 });
 
 export default GameScreen;
+
+//useRef===> allows to create an object which you can bind to inputs
+// and we can access to them in our code
+//===> allows to define a value which survives component re-renders
