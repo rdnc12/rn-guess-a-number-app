51ebc11ca931ad817bf0451f2689ec6a399ecb63
Rdnc12 <rdnc12@gmail.com>
2021-04-12 18:17:01 +0200

    scrollview and flexbox

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -79,8 +79,8 @@ const GameScreen = ({ userChoice, onGameOver }) => {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.list}>
-        <ScrollView>
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) =>
             renderListItem(guess, pastGuesses - index)
           )}
@@ -108,9 +108,14 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
   },
-  list: {
-    flex:1, 
+  listContainer: {
+    flex: 1,
     width: "80%",
+  },
+  list: {
+    flexGrow:1,
+    alignItems: "center",
+    justifyContent:'flex-end'
   },
   listItem: {
     borderColor: "#ccc",
@@ -119,7 +124,7 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     backgroundColor: "white",
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
   },
 });
 
