cad09fae5cf35d736b5e716d1427a334b2b38dda
Rdnc12 <rdnc12@gmail.com>
2021-04-14 18:14:49 +0200

    platform api

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,47 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 import TitleText from "./TitleText";
 import Colors from "../constants/colors";
 
 const Header = ({ title }) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
       <TitleText style={styles.headerTitle}>{title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: "100%",
     height: 90,
     padding: 36,
-    backgroundColor: Colors.primary,
     alignItems: "center",
     justifyContent: "center",
+    // backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
+    // borderBottomColor: Platform.OS === "ios" ? "#ccc" : "transparent",
+    // borderBottomWidth: Platform.OS === "ios" ? 1 : 0,
+  },
+  headerIOS: {
+    backgroundColor: "white",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+    borderBottomColor: "transparent",
   },
   headerTitle: {
-    color: "black",
+    color: Platform.OS === "ios" ? "black" : "white",
   },
 });
 
--- components/MainButton.js
+++ components/MainButton.js
@@ -1,32 +1,53 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 
 import Colors from "../constants/colors";
 
 const MainButton = ({ children, onPress }) => {
+  let ButtonComponent = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.6} onPress={onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer:{
+    borderRadius: 25,
+    overflow:'hidden'
+  },
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
     paddingHorizontal: 30,
     borderRadius: 25,
-    marginTop:10
+    marginTop: 10,
   },
   buttonText: {
     color: "white",
     fontFamily: "open-sans",
     fontSize: 18,
-    textAlign:'center'
+    textAlign: "center",
   },
 });
 
 export default MainButton;
+
+//TouchableNativeFeedback ===> gives us a touchable object which has
+// a built-in ripple effect . This is for Android
+//TouchableOpacity==> For ios
